b5a495f8a7a601a3d2e139694d0f9468855caf4d
r0982289 <blesoprincess@gmail.com>
2024-12-20 23:14:17 +0100

    put , delete

--- back-end/controller/crop.routes.ts
+++ back-end/controller/crop.routes.ts
@@ -1,32 +1,3 @@
-/**
- * @swagger
- * components:
- *   securitySchemes:
- *     bearerAuth:
- *       type: http
- *       scheme: bearer
- *       bearerFormat: JWT
- *   schemas:
- *     Crop:
- *       type: object
- *       properties:
- *         name:
- *           type: string
- *           description: Crop name.
- *         purchasePrice:
- *           type: number
- *           description: Crop purchase price.
- *         marketPrice:
- *           type: number
- *           description: Crop market price.
- *         attentionRange:
- *           type: number
- *           description: Crop attention range.
- *         growthDurationInMonths:
- *           type: number
- *           description: Crop growth duration.
- */
-
 import express, { NextFunction, Request, Response } from 'express';
 import cropService from '../service/crop.service';
 
@@ -47,7 +18,6 @@ const cropRouter = express.Router();
  *             schema:
  *               $ref: '#/components/schemas/Crop'
  */
-
 cropRouter.get('/', async (req: Request, res: Response, next: NextFunction) => {
     try {
         const crops = await cropService.getAllCrops();
@@ -79,11 +49,85 @@ cropRouter.get('/', async (req: Request, res: Response, next: NextFunction) => {
  *             schema:
  *               $ref: '#/components/schemas/Crop'
  */
-
 cropRouter.get('/:id', async (req: Request, res: Response, next: NextFunction) => {
     try {
         const crop = await cropService.getCropById(Number(req.params.id));
         res.status(200).json(crop);
+    } catch (error) {
+        next(error);
+    }
+});
+
+/**
+ * @swagger
+ * /crops/{id}:
+ *   put:
+ *     security:
+ *       - bearerAuth: []
+ *     summary: Update a crop by id.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: The crop id.
+ *       - in: body
+ *         name: crop
+ *         description: The updated crop object.
+ *         required: true
+ *         content:
+ *           application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Crop'
+ *     responses:
+ *       200:
+ *         description: The updated crop object.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Crop'
+ */
+cropRouter.put('/:id', async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const updatedCrop = req.body;
+        const crop = await cropService.updateCrop(Number(req.params.id), updatedCrop);
+        if (!crop) {
+            res.status(404).json({ message: 'Crop not found' });
+        } else {
+            res.status(200).json(crop);
+        }
+    } catch (error) {
+        next(error);
+    }
+});
+
+/**
+ * @swagger
+ * /crops/{id}:
+ *   delete:
+ *     security:
+ *       - bearerAuth: []
+ *     summary: Delete a crop by id.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: The crop id.
+ *     responses:
+ *       200:
+ *         description: Crop successfully deleted.
+ */
+cropRouter.delete('/:id', async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const result = await cropService.deleteCrop(Number(req.params.id));
+        if (result) {
+            res.status(200).json({ message: 'Crop successfully deleted' });
+        } else {
+            res.status(404).json({ message: 'Crop not found' });
+        }
     } catch (error) {
         next(error);
     }
